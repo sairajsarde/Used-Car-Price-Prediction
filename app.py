ad3f30a80717759ef3d4b8ce61c68c6ddb4990d0
Sairaj <sairaj1637@gmail.com>
2021-11-30 13:13:03 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model=pickle.load(open('model.pkl','rb'))
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[float(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    print(int_features)
+    print(final)
+    prediction=model.predict(final)
+    output=round(prediction[0], 2)
+    return render_template('index.html',pred='Car Price is  {}'.format(output))
+     
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
